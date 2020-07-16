1752e819579a7b8dc90c3ec7939e3cec52f29e4b
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-07-17 04:53:43 +0500

    Chapter #43-48 and Task #2 complete

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('Javascript Assignment #9');
 
 // FUNCTIONS, switch statements, while... do-while loops | JAVASCRIPT
 
-// Assignment # 38-44 task #1
+// Chapter #38-44 and Task #1
 
 function power(base, exponent) {
 	var result = 1;
@@ -17,10 +17,22 @@ function power(base, exponent) {
 console.log(power(2,3));
 console.log(power(5));
 
-// Assignment # 38-44 task #2
+// Chapter #38-44 and Task #2
 
 function isLeapYear() { 
     var year= document.getElementById("year").value; 
       
     document.getElementById("displayAnswer").innerHTML = (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0); 
 } 
+
+// Chapter #43-48 and Task #1
+
+ function greetUser() {
+     alert("Hello User")
+ }
+
+// Chapter #43-48 and Task #2
+
+function showTheMessage() {
+    alert('Thanks for purchasing a phone from us');
+}
