e0e4826713f37aec199615843cf4f111f900c7b7
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-07-18 01:19:02 +0500

    Chapter #38-44 and Task #3 and 4 complete

--- app.js
+++ app.js
@@ -22,6 +22,48 @@ function isLeapYear() {
     document.getElementById("displayAnswer").innerHTML = (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0); 
 } 
 
+// Chapter #38-44 and Task #3
+
+function areaOfTriangle(a, b, c) {
+ var s = ( a + b + c ) / 2;     
+
+   return s*(s - a)*(s - b)*(s - c);     
+		
+}
+ 
+  document.write("<br />" + "Area of Triangle is: "  + areaOfTriangle(2, 5, 6));
+
+// Chapter #38-44 and Task #4
+    
+   function averageFunction(subject1, subject2, subject3) {
+      var averageInThreeSubjects = subject1 + subject2 + subject3 / 3;
+	  
+	  return averageInThreeSubjects;	   
+   }
+
+   function percentageFunction(subject1, subject2, subject3) {
+	  
+	var obtainedMarks = subject1 + subject2 + subject3;
+
+	var percentage = obtainedMarks * 100 / 300;
+
+	return percentage;
+
+}
+
+   function mainFuntion(sub1 ,sub2 ,sub3) {
+
+   var percentage =	percentageFunction(sub1, sub2, sub3)
+   var average = averageFunction(sub1 ,sub2 ,sub3)
+   
+    return "<br />" + 'Percentage in three subject ' + percentage + '%' + "<br />" + 'Average marks in three subject ' + average;
+
+	
+}
+
+   document.write(mainFuntion(70, 80, 60))
+
+
 // Chapter #43-48 and Task #1
 
  function greetUser() {
@@ -73,14 +115,14 @@ var modal = document.getElementById("modal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+// var img = document.getElementById("myImg");
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
